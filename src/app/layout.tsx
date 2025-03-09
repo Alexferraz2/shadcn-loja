9b48f11962d68c7a34386f004e63c1c9bb587e5c
Alexferraz2 <alex_ferraz2@hotmail.com>
2025-03-09 19:30:23 -0300

    Criado a função de listar os produtos e botão de Toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -35,7 +36,9 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             {children}
+            <Toaster />
           </ThemeProvider>
+          
       </body>
     </html>
   );
--- src/components/products/tab.tsx
+++ src/components/products/tab.tsx
@@ -1,24 +1,64 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { getAllProducts } from "@/services/products";
+import { Product } from "@/types/product";
+import { ProductEmpty } from "./empty";
+import { ProductItem } from "./item";
 
-const ProductsTab = () => {
+type Tab = {
+    title: string,
+    value: string,
+    products: Product[]
+}
+
+const ProductsTab = async () => {
+
+    const products = await getAllProducts();
+
+    const tabs: Tab[] = [
+        {
+            title: 'Sushi',
+            value: 'sushi',
+            products: products.filter(item => item.category === "sushi")
+        },
+        {
+            title: 'Temaki',
+            value: 'Temaki',
+            products: products.filter(item => item.category === "temaki")
+        },
+        {
+            title: 'Combinados',
+            value: 'pack',
+            products: products.filter(item => item.category === "pack")
+        },
+        {
+            title: 'Bebidas',
+            value: 'beverage',
+            products: products.filter(item => item.category === "beverage")
+        }
+    ];
     return(
-        <Tabs defaultValue="tab1">
+        <Tabs defaultValue="sushi">
             <TabsList className="flex">
-                <TabsTrigger 
-                    className="flex-1"
-                    value="tab1">
-                    Tab 1
-                </TabsTrigger>
+                {tabs.map(item => (
+                    <TabsTrigger key={item.value} value={item.value} className="flex-1">
+                        {item.title}
+                    </TabsTrigger>
+                ))}
 
-                <TabsTrigger
-                     className="flex-1"
-                     value="tab2">
-                    Tab 2
-                </TabsTrigger>
+                
             </TabsList>
-            <TabsContent value="tab1" className="mt-6">
-                Conteúdo do Tab1
-            </TabsContent>
+            {tabs.map(item => (
+                <TabsContent key={item.value} value={item.value} className="mt-6">
+                    {item.products.length > 0 && 
+                        <div className="grid gap-5 grid-cols-2 sm:grid-cols-3 md:grid-cols-4">
+                            {item.products.map(product => 
+                                <ProductItem key={product.id} item={product}/>
+                            )}
+                        </div>
+                    } 
+                    {item.products.length === 0 && <ProductEmpty/>}
+                </TabsContent>
+            ))}
             <TabsContent value="tab2" className="mt-6">
                 Conteúdo do Tab2
             </TabsContent>
